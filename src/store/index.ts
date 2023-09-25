5df4dc187f2e15a13fad2aa0c802b17fe4a3058d
Vitor Soares <vitorsoaresf@gmail.com>
2023-09-25 08:06:12 -0300

    feat: implements store vuex

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import IProjeto from "@/interfaces/IProjeto";
+import { createStore, Store } from "vuex";
+import { InjectionKey } from "vue";
+
+interface IEstado {
+  projetos: IProjeto[];
+}
+
+export const key: InjectionKey<Store<IEstado>>;
+
+export const store = createStore<IEstado>({
+  state: {
+    projetos: [
+      {
+        id: new Date().toISOString(),
+        nome: "Typescript",
+      },
+    ],
+  },
+});
